8785141d913f1f7fa91381623e7e8d66ef1d6014
mpower <mpowerpc@gmail.com>
2025-02-22 11:50:31 -0500

    Added pihole gravity update

--- update.sh
+++ update.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-python3 update_lists.py
-
 git pull
+
+python3 update_lists.py
 
 if [ -n "$(git status --porcelain)" ]; then
   git add .
   git commit -m "Update tracker lists on $(date +'%Y-%m-%d %H:%M:%S')"
   git push origin main
   echo "Updates have been committed and pushed to the main branch."
+
+  PIHOLE_API_URL="http://$PIHOLE_IP_ADDRESS/admin/api.php?auth=$PIHOLE_API_TOKEN&action=reload"
+  curl -X GET "$PIHOLE_API_URL"
+
+  echo "Pi-hole Gravity lists have been updated."
 else
   echo "No changes to commit."
 fi
