f6a1fa2859b64370f740c41e7d57668003a81ff7
mpower <mpowerpc@gmail.com>
2025-02-22 12:44:58 -0500

    Removed pihole api.

--- update_and_commit.sh
+++ update_and_commit.sh
@@ -13,11 +13,6 @@ if [ -n "$(git status --porcelain)" ]; then
   git commit -m "Update tracker lists on $(date +'%Y-%m-%d %H:%M:%S')"
   git push origin main
   echo "Updates have been committed and pushed to the main branch."
-
-  PIHOLE_API_URL="http://$PIHOLE_IP_ADDRESS/admin/api.php?auth=$PIHOLE_API_TOKEN&action=reload"
-  curl -X GET "$PIHOLE_API_URL"
-
-  echo "Pi-hole Gravity lists have been updated."
 else
   echo "No changes to commit."
 fi
